b6a2c2d7f7b40144fcbf8363e924414b8135da89
kfleischhacker <kfleischhacker@andromeda.software>
2020-01-17 17:33:59 +0100

    fixed styling in Navigation.tsx / index.tsx, added .gitignore, added unversioned files

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,39 +1,24 @@
 import React from "react";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/core";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import BestPlayersPage from "../pages/BestPlayersPage";
-import LandingPage from "../pages/LandingPage";
-import HomePage from "../pages/HomePage";
+import { Link } from "react-router-dom";
+import {
+  Flex,
+  Box,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel
+} from "@chakra-ui/core";
+
 import * as ROUTES from "../routes";
 
 const Navigation = () => (
-  <Tabs>
-    <Router>
-      <TabList>
-        <Tab>
-          <Link to={ROUTES.LANDING}>Landing Page</Link>
-        </Tab>
-        <Tab>
-          <Link to={ROUTES.HOME}>Home</Link>
-        </Tab>
-        <Tab>
-          <Link to={ROUTES.BEST_PLAYERS}>Best Players</Link>
-        </Tab>
-      </TabList>
-
-      <TabPanels>
-        <TabPanel>
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-        </TabPanel>
-        <TabPanel>
-          <Route path={ROUTES.HOME} component={HomePage} />
-        </TabPanel>
-        <TabPanel>
-          <Route path={ROUTES.BEST_PLAYERS} component={BestPlayersPage} />
-        </TabPanel>
-      </TabPanels>
-    </Router>
-  </Tabs>
+  <Flex px={2} color="white" bg="black" alignItems="center">
+    <Link to={ROUTES.LANDING}>Landing Page</Link>
+    <Box mx="auto" />
+    <Link to={ROUTES.HOME}>Home</Link>
+    <Link to={ROUTES.BEST_PLAYERS}>Best Players</Link>
+  </Flex>
 );
 
 export default Navigation;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import App from "./App";
 import Navigation from "./components/Navigation";
+import LandingPage from "./pages/LandingPage";
+import HomePage from "./pages/HomePage";
+import BestPlayersPage from "./pages/BestPlayersPage";
+
+import * as ROUTES from "./routes";
 
 ReactDOM.render(
   <App>
-    <Navigation />
+    <Router>
+      <Navigation />
+      <hr />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.BEST_PLAYERS} component={BestPlayersPage} />
+    </Router>
   </App>,
   document.getElementById("root")
 );
